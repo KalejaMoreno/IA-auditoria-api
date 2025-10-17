a1699c7b736367c6e2a3bda529b5ae6f640155f8
KalejaMoreno <kmoreno@sdsigma.com>
2025-10-17 12:22:21 -0500

    Ajuste puerto para Render

--- main.py
+++ main.py
@@ -206,6 +206,10 @@ async def procesar_archivo(file: UploadFile = File(...)):
 # ==============================
 # PRUEBAS LOCALES
 # ==============================
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+#if __name__ == "__main__":
+#    import uvicorn
+#    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+port = int(os.environ.get("PORT", 8000))
+uvicorn.run(app, host="0.0.0.0", port=port)
+
